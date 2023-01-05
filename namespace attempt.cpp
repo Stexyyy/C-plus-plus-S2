3982b2a3023cfc507eecf1fbf4fc189eb17d1dd6
Stexyyy <78552805+Stexyyy@users.noreply.github.com>
2023-01-05 08:36:23 -0700

    Create namespace attempt.cpp

---
+++ namespace attempt.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+namespace hi {
+	void pow(float s, float b) {
+		
+	}
+}
+
+int main() {
+
+	float x = 2.46;
+	float y = 3.59;
+	float answer = pow(x, y);
+	cout << fixed << setprecision(5) << answer;
+
+}
